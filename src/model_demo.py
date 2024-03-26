524dddae9924fc8496380199834d85c2bba57cba
wongyufei1 <116786597+wongyufei1@users.noreply.github.com>
2024-03-26 21:04:59 +0800

    updated with spacy display

--- src/model_demo.py
+++ src/model_demo.py
@@ -3,8 +3,10 @@ import os
 import sys
 import warnings
 
+import spacy
 import torch
 from ipymarkup import show_span_ascii_markup
+from spacy import displacy
 from transformers import AutoTokenizer, AutoModelForTokenClassification, pipeline
 from transformers.utils import logging
 
@@ -23,8 +25,14 @@ def run():
     print("Input text:")
     print(text)
 
+    nlp = spacy.blank("en")
+    docs = []
+
     for m in MODEL_PATHS:
         print(f"\n---------------------- Running Model for Inference (Model:{m['name']}) ----------------------")
+
+        doc = nlp.make_doc(text)
+
         tokenizer = AutoTokenizer.from_pretrained(os.path.join(OUT_DIR, f"{m['name']}/save_model"))
         model = AutoModelForTokenClassification.from_pretrained(os.path.join(OUT_DIR, f"{m['name']}/save_model"))
 
@@ -32,15 +40,28 @@ def run():
         out = pipe(text)
 
         spans = []
+        ents = []
+
         for prediction in out:
-            spans.append((prediction["start"], prediction["end"], prediction["entity_group"]))
+            span = (prediction["start"], prediction["end"], prediction["entity_group"])
+            ent = doc.char_span(span[0], span[1], span[2])
+
+            if ent is None:
+                continue
+
+            spans.append(span)
+            ents.append(ent)
+
+        doc.ents = ents
+        docs.append(doc)
+        show_span_ascii_markup(text, spans)
 
         # clear gpu cache for OOM problem
         del tokenizer, model
         gc.collect()
         torch.cuda.empty_cache()
 
-        show_span_ascii_markup(text, spans)
+    displacy.serve(docs, style="ent", host="127.0.0.1", port=5000)
 
 
 if __name__ == "__main__":
